bdfe2f8f36898bf1895c2a434901f5a32bdcdeb9
grant-w <trduunze@gmail.com>
2020-06-12 09:59:20 -0500

    Created basic store UI layout

---
+++ Hunt The Wumpus/Assets/Scripts/Gui/Store.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gui
+{
+    public class Store : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        public void BuyNuke()
+        {
+            print("Buyng Nuke");
+        }
+    }
+}
