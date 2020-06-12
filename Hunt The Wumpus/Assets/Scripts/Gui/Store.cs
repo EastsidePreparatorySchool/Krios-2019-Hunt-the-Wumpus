a6ca7eedd388eb1508f4580fa5748219a85c0d17
grant-w <trduunze@gmail.com>
2020-06-12 11:32:22 -0500

    created functions for all store buttons, "buy nuke" fully implement

--- Hunt The Wumpus/Assets/Scripts/Gui/Store.cs
+++ Hunt The Wumpus/Assets/Scripts/Gui/Store.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using CommandView;
 
 namespace Gui
 {
     public class Store : MonoBehaviour
     {
+        private GameObject _planet;
+        private Planet _planetHandler;
+        private GameMeta _gameMeta;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            _planet = GameObject.Find("Planet");
+            _planetHandler = _planet.GetComponent<Planet>();
+            _gameMeta = _planet.GetComponent<GameMeta>();
         }
 
         // Update is called once per frame
@@ -18,9 +25,50 @@ namespace Gui
         {
         }
 
+        int troopDmgIncrease = 1;
+        int troopDmgCost = 1;
+        int NukeCost = 1;
+        int SensorTowerCost = 1;
+
+        public void BuyTroops()
+        {
+            print("buying troops");
+        }
+
+        public void BuyTroopDmg()
+        {
+            if (_gameMeta.money >= troopDmgCost)
+            {
+                print("Buyng Damage Upgrade");
+                _gameMeta.money -= troopDmgCost;
+                //_gameMeta.nukes++;
+            }
+            else
+                print("Not enough money");
+        }
+
         public void BuyNuke()
         {
-            print("Buyng Nuke");
+            if (_gameMeta.money >= NukeCost)
+            {
+                print("Buyng Nuke");
+                _gameMeta.money -= NukeCost;
+                _gameMeta.nukes++;
+            }
+            else
+                print("Not enough money");
+        }
+
+        public void BuySensorTower()
+        {
+            if (_gameMeta.money >= SensorTowerCost)
+            {
+                print("Buyng Sensor Tower");
+                _gameMeta.money -= SensorTowerCost;
+                //_gameMeta.sensor....++;
+            }
+            else
+                print("Not enough money");
         }
     }
 }
