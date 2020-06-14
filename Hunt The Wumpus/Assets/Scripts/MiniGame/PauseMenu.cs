de9bc421e813bcdace8162826c67a29a2de466be
grant-w <trduunze@gmail.com>
2020-06-14 11:51:17 -0500

    pause menu

---
+++ Hunt The Wumpus/Assets/Scripts/MiniGame/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void LoadGame()
+    {
+        print("make tis actually load a game save");
+    }
+
+    public void SaveQuit()
+    {
+        print("make tis actually save an quit");
+    }
+}
