b109b72c2e149a98216954b0d68d9019e03d54ec
grant-w <trduunze@gmail.com>
2020-07-17 13:35:48 -0500

    Custom cursor working in command view with one cursor

--- Hunt The Wumpus/Assets/Scripts/Gui/CursorController.cs
+++ Hunt The Wumpus/Assets/Scripts/Gui/CursorController.cs
@@ -5,41 +5,20 @@ namespace Gui
 {
     public class CursorController : MonoBehaviour
     {
-        public Image img;
-        //public Camera mc;
-
-        private Transform _myTransform;
+        //public Image img;
         
         // Start is called before the first frame update
         void Start()
         {
-            //Cursor.visible = false;
+            Cursor.visible = false;
             //img = GetComponent<Image>();
-            _myTransform = transform;
 
         }
 
         // Update is called once per frame
         void Update()
         {
-            /*Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        cursorPos.z = -12;
-        transform.position = cursorPos;*/
-
-            Vector3 mousePos = Input.mousePosition;
-            // _myTransform.position = mousePos;
-            mousePos.z = 12;
-            if (Camera.main != null)
-            {
-                Vector3 newpos = Camera.main.ScreenToWorldPoint(mousePos);
-                //newpos.x *= 3;
-                //newpos.x += 19;
-                //newpos.y *= 3;
-                //ewpos.y += 2;
-                Debug.Log(newpos);
-
-                _myTransform.position = new Vector3(newpos.x, newpos.y, transform.position.z - 14);
-            }
+            transform.position = Input.mousePosition;
         }
     }
 }
