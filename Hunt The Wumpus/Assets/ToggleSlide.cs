32aa192d0979e2d40c41da8fb0ec14c48113ee8f
grant-w <trduunze@gmail.com>
2020-07-18 18:09:45 -0500

    Major UI updates part 1

---
+++ Hunt The Wumpus/Assets/ToggleSlide.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSlide : MonoBehaviour
+{
+    public float distance;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void SetToggle(bool isOn)
+    {
+        if (isOn)
+            LeanTween.moveLocalX(gameObject, distance, speed);
+        else
+            LeanTween.moveLocalX(gameObject, -distance, speed);
+    }
+}
