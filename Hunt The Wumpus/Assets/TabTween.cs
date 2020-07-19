8406d3308b62979c5f0e4775e1c5ef56d3b16128
grant-w <trduunze@gmail.com>
2020-07-19 00:53:06 -0500

    Major UI updates part 2

---
+++ Hunt The Wumpus/Assets/TabTween.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabTween : MonoBehaviour
+{
+    public LeanTweenType ease;
+    public float distance;
+    public float speed;
+
+    public void TabChange(bool firstTab)
+    {
+        if (firstTab)
+            LeanTween.moveLocalX(gameObject, -distance, speed).setEase(ease);
+        else
+            LeanTween.moveLocalX(gameObject, distance, speed).setEase(ease);
+    }
+}
