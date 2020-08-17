5bda14696f58cf62ebfe394ab69afd937592d7bf
Kenneth Y <kyang@eastsideprep.org>
2020-08-17 16:54:56 -0700

    Coin popping

---
+++ Hunt The Wumpus/Assets/Scripts/CoinPop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.rotate(gameObject, new Vector3(0, 360 * 5, 0), 1f);
+
+        float initY = transform.position.y;
+
+        var seq = LeanTween.sequence();
+        seq.append(LeanTween.moveY(gameObject, initY + 5, 0.6f));
+        seq.append(LeanTween.moveY(gameObject, initY, 0.4f));
+        Destroy(gameObject, 1f);
+    }
+}
